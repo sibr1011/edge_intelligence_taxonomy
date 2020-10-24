5875db14426cf7fbf76994b9f6d86fd91e7636e9
sibr1011 <siegel.brian93@gmail.com>
2020-10-25 00:10:16 +0200

    .R file year publication count

---
+++ published_year_count.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(ggplot2)
+
+text <- "./final_summary.csv"
+
+literature <- read.csv2(text)
+
+position = position_dodge(width = .75)
+width = .75
+
+p <- literature %>% 
+  filter(Year<2021) %>% 
+  ggplot(aes(x=Year)) +
+  geom_bar(fill="gray", color="black") +
+  geom_text(stat='count', aes(label=..count..), vjust=-1) +
+  xlab("Year of Publication") + ylab("Counts") +
+  theme_bw()
+p
